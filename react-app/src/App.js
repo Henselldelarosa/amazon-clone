4754e1cba70487450d6108d1814b564cb70ce042
hensell de la rosa <henseldelarosa@DESKTOP-DOB1QI3.localdomain>
2023-12-05 01:45:59 -0500

    frontend payment process done

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,6 +5,10 @@ import { Home, Checkout,Login, Payment } from "./exports";
 import {auth} from './firebase'
 import { useStateValue } from "./context/StateProvider";
 
+import {loadStripe} from '@stripe/stripe-js'
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe("pk_test_51OJqDqAzbdz6FzdbkXg0kwe3HFARvi5ynJ7N3zrULBna6Ro4wChT2Abt22cAnDiqpxZn045RzGUcyIRASZMaHRdj00a542pCzP")
 
 function App() {
   const [{}, dispatch] = useStateValue()
@@ -41,7 +45,11 @@ function App() {
           </Route>
 
           <Route path='/payment'>
-            <Payment/>
+
+            <Elements stripe={promise}>
+              <Payment/>
+            </Elements>
+            
           </Route>
 
           <Route path='/'>
--- react-app/src/pages/payment/Payment.js
+++ react-app/src/pages/payment/Payment.js
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Payment.scss'
 import { useStateValue } from '../../context/StateProvider'
 import { CheckoutProduct } from '../../exports'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import { Link, useHistory } from 'react-router-dom/cjs/react-router-dom.min'
+import { useElements, useStripe, CardElement } from '@stripe/react-stripe-js'
+import CurrencyFormat from 'react-currency-format'
+import { getBasketTotal } from '../../store/reducer'
+import axios from '../../axios'
+
 
 const Payment = () => {
   const [{basket, user}, dispatch] = useStateValue()
+  const history = useHistory()
+  const stripe = useStripe()
+  const elements = useElements()
+
+  const [error, setError] = useState(null)
+  const [disabled, setDisabled] = useState(true)
+  const [succeeded, setSucceeded] = useState(false)
+  const [processing, setProcessing] = useState(false)
+  const [clientSecret, setClientSecret] = useState(true)
+
+  useEffect(() => {
+    const getClientSecret = async () => {
+      const response = await axios({
+        method: 'post',
+        // *100 because stripe process the payments in subunits(cents)
+        url: `/payments/create?total=${getBasketTotal(basket) * 100}`
+      })
+
+      setClientSecret(response.data.clientSecret)
+    }
+  },[basket])
+
+  const handlePayment = async (e) => {
+    e.preventDefault()
+    setProcessing(true)
+
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement)
+      }
+    }).then(({paymentIntent}) => {
+      setSucceeded(true)
+      setError(null)
+      setProcessing(false)
+      // history.replace will replace the current page
+      history.replace('/orders')
+    })
+
+  }
+
+
+  const handleChange = (e) => {
+    setDisabled(e.empty)
+    setError(e.error? e.error.message : '')
+  }
+
   return (
     <div className='payment'>
 
@@ -53,6 +104,30 @@ const Payment = () => {
           </div>
 
           <div className="payment__detail">
+
+            <form onSubmit={handlePayment}>
+              <CardElement onChange={handleChange}/>
+
+              <div className="payment__priceContainer">
+
+                <CurrencyFormat
+                  renderText={(value) => (
+                    <h3> Order Total: <strong>{value}</strong></h3>
+                    )}
+                  decimalScale={2}
+                  value={getBasketTotal(basket)}
+                  displayType={'text'}
+                  thousandSeparator = {true}
+                  prefix={'$'}
+                  />
+
+                  <button type='submit' disabled ={processing || disabled || succeeded}>
+                    <span>{processing? <p>Processing</p> : "Buy Now"}</span>
+                  </button>
+              </div>
+
+              {error && <div>{error}</div>}
+            </form>
 
           </div>
         </div>
