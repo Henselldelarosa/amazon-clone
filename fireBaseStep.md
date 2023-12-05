51f2dc9ee2f51cea46f606cef0aec4c9d45aaae6
hensell de la rosa <henseldelarosa@DESKTOP-DOB1QI3.localdomain>
2023-12-05 03:05:28 -0500

    started backend

--- fireBaseStep.md
+++ fireBaseStep.md
@@ -3,12 +3,15 @@ npm install firebase
 sudo npm install -g firebase-tools
 firebase login
 firebase init
-<!-- choose the hosting: Config -->
+<!-- Hosting: Configure files for Firebase Hosting and (optionally) set up GitHub Action deploys -->
 <!-- use Existing project -->
 <!-- choose the project -->
 <!-- where it says public change to 'build' -->
 <!-- single page application 'yes' -->
 npm run build
+firebase deploy
+
+<!-- everytime changes are made to the app must run npm run build and then firebase deploy -->
 
 next
 npm firebase init
--- react-app/functions/index.js
+++ react-app/functions/index.js
@@ -1,3 +1,30 @@
+const functions = require('firebase-functions')
+const express = require('express')
+const cors = require('cors')
+const stripe = require('stripe')('sk_test_51OJqDqAzbdz6FzdbvGSN1S6hW8MRnlsQXQl056sxkeKCeWhsHgODlI2Ugq9RPOLk6tWlt6snUjkPjyvkrTSvbdFa00aIV2RA6E')
+
+//API SET UP
+
+//app config
+const app = express()
+
+//Middlewares
+app.use(cors({origin:true}))
+app.use(express.json())
+
+//API routes
+app.get('/', (req, res) => res.status(200).send('Hello World'))
+
+app.post('/payments/create', async(req, res) => {
+
+  const total = req.query.total
+
+})
+
+//Listen command
+
+exports.api = functions.https.onRequest(app)
+
 /**
  * Import function triggers from their respective submodules:
  *
@@ -7,8 +34,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
